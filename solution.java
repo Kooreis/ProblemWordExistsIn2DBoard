138830d36e9322730439e9dd2ec21f19f357a44c
Kooreis <kooreis.official@gmail.com>
2024-10-04 13:37:56 -0500

    Defines the main method which takes user input for the number of rows and columns, the characters in the board, and the word to search. Then it calls the 'exist' method with the board and the word as arguments.

--- solution.java
+++ solution.java
@@ -1,5 +1,17 @@
-import java.util.Scanner;
-
-public class Main {
-    private static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-    private static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter number of columns:");
+        int cols = scanner.nextInt();
+        char[][] board = new char[rows][cols];
+        System.out.println("Enter the characters in the board:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                board[i][j] = scanner.next().charAt(0);
+            }
+        }
+        System.out.println("Enter the word to search:");
+        String word = scanner.next();
+        System.out.println(exist(board, word));
+    }
